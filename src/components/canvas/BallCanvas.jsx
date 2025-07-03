24f51e13f69348c01ab648a00e4ee1fbedf7afc4
divyam007142 <divyamsingha22@gmail.com>
2025-07-04 01:05:06 +0530

    Update BallCanvas.jsx

--- src/components/canvas/BallCanvas.jsx
+++ src/components/canvas/BallCanvas.jsx
@@ -1,21 +1,32 @@
-// src/components/canvas/BallCanvas.jsx
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload } from "@react-three/drei";
+
 import Ball from "./Ball";
 import CanvasLoader from "../Loader";
 import { technologies } from "../../constants";
 
 const BallCanvas = () => {
   return (
-    <Canvas frameloop="demand" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{ position: [0, 0, 10], fov: 50 }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 1]} />
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         {technologies.map((tech, i) => (
           <Ball
             key={tech.name}
             icon={tech.icon}
-            position={[i * 2 - 7, 0, 0]} // spread them horizontally
+            position={[
+              (i % 4) * 3 - 4.5, // 4 per row
+              -Math.floor(i / 4) * 3, // new row every 4
+              0
+            ]}
           />
         ))}
       </Suspense>
