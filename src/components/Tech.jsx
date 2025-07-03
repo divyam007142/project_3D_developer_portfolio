97c247607d855d95ab8826120c06dbefebbc8ed4
divyam007142 <divyamsingha22@gmail.com>
2025-07-04 02:08:10 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
-  Float,
   OrbitControls,
   Preload,
   useTexture,
@@ -12,42 +11,36 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import CanvasLoader from "./Loader";
 
-const Ball = ({ imgUrl, position }) => {
+const FlatBall = ({ imgUrl, position }) => {
   const [decal] = useTexture([imgUrl]);
 
   return (
-    <Float
-      speed={2}
-      rotationIntensity={2}
-      floatIntensity={3}
-    >
-      <mesh position={position} scale={1.5} castShadow receiveShadow>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+    <mesh position={position} scale={1.5}>
+      <icosahedronGeometry args={[1, 1]} />
+      <meshStandardMaterial
+        color='#fff8eb'
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+      />
+      <Decal
+        position={[0, 0, 1]}
+        rotation={[0, 0, 0]}
+        scale={1}
+        map={decal}
+        flatShading
+      />
+    </mesh>
   );
 };
 
 const Tech = () => {
-  const spacingX = 4; // horizontal gap
-  const spacingY = 4; // vertical gap
-  const perRow = 4;   // how many balls per row
+  const spacingX = 4; // Horizontal gap
+  const spacingY = 4; // Vertical gap
+  const perRow = 4;   // Number per row
 
   return (
-    <div className="w-full h-[500px]"> {/* Fix height to contain canvas */}
+    <div className="w-full h-[500px]">
       <Canvas
         camera={{ position: [0, 0, 20], fov: 50 }}
         gl={{ preserveDrawingBuffer: true }}
@@ -55,18 +48,19 @@ const Tech = () => {
         <Suspense fallback={<CanvasLoader />}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[0, 5, 5]} intensity={0.5} />
-          <OrbitControls enableZoom={false} />
+
+          {/* OPTIONAL: If you want user to orbit */}
+          <OrbitControls enableZoom={false} enableRotate={false} />
 
           {technologies.map((tech, index) => {
             const row = Math.floor(index / perRow);
             const col = index % perRow;
 
-            // Spread out balls nicely
             const posX = (col - (perRow - 1) / 2) * spacingX;
             const posY = -row * spacingY + 3;
 
             return (
-              <Ball
+              <FlatBall
                 key={tech.name}
                 imgUrl={tech.icon}
                 position={[posX, posY, 0]}
