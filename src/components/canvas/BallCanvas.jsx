c55a1ecc7cbafa38dc07f230707494c56609beca
divyam007142 <divyamsingha22@gmail.com>
2025-07-04 01:32:56 +0530

    Update BallCanvas.jsx

--- src/components/canvas/BallCanvas.jsx
+++ src/components/canvas/BallCanvas.jsx
@@ -1,21 +1,25 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload } from "@react-three/drei";
+
 import Ball from "./Ball";
 import CanvasLoader from "../Loader";
 
-const BallCanvas = ({ icon }) => (
-  <Canvas
-    frameloop="always"
-    dpr={[1, 2]}
-    gl={{ preserveDrawingBuffer: true }}
-  >
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls enableZoom={false} />
-      <Ball imgUrl={icon} />
-    </Suspense>
-    <Preload all />
-  </Canvas>
-);
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball icon={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
 
 export default BallCanvas;
