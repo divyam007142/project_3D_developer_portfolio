ef6f26ebf9dd06dfc78098d71ccee22651349fc2
divyam007142 <divyamsingha22@gmail.com>
2025-07-04 01:44:47 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,65 @@
-import React from "react";
-import { technologies } from "../constants";
-import BallCanvas from "./canvas/BallCanvas";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
 import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import CanvasLoader from "../components/Loader";
+
+const Ball = ({ imgUrl, position }) => {
+  const [decal] = useTexture([imgUrl]);
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh position={position} scale={2.75} castShadow receiveShadow>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
+};
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        {technologies.map((tech, index) => (
+          <Ball
+            key={tech.name}
+            imgUrl={tech.icon}
+            // Example positions in a grid row
+            position={[index * 3 - 12, 0, 0]}
+          />
+        ))}
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   );
 };
 
