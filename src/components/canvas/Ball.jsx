745a29fc9e8da2ef154d53eecd046bd9d7525fea
divyam007142 <divyamsingha22@gmail.com>
2025-07-04 01:10:27 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Decal, Float, useTexture } from "@react-three/drei";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CanvasLoader from "../Loader";
 
-const Ball = ({ icon, position = [0, 0, 0] }) => {
-  const [decal] = useTexture([icon]);
-
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <mesh position={position} castShadow receiveShadow scale={2.5}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.5}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color="#fff8eb"
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
